2e83b2c4f17912db99da3c6a1c89d0fa784f2c2d
Josh Henry <joshhenryjc@gmail.com>
2022-01-09 22:29:57 -0600

    Add strapi-parse.js

--- README.md
+++ README.md
@@ -1,2 +1,94 @@
+![Version](https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://github.com/jch-code/strapi-parse/blob/main/LICENSE)
+
 # strapi-parse
-Parse Strapi V4 API responses into easier to work with objects
+
+> NPM Package to parse Strapi V4 API responses into easier to work with objects.
+
+## Usage
+
+```sh
+$ npm i --save strapi-parse
+```
+
+```javascript
+import { strapiParse } from "strapi-parse";
+
+const parsedAPIResponse = strapiParse(strapiAPIResponse);
+```
+
+## Examples
+> strapi-parse does not currently support getting data from the meta properties
+### Collection Type
+```javascript
+// Strapi API Response
+var strapiAPIResponse = {
+  data: [
+    {
+      id: 1,
+      attributes: {
+        title: Test Article,
+        slug: test-article,
+        // ...
+      }
+    },
+    {
+      id: 2,
+      attributes: {
+        title: Test Article 2,
+        slug: test-article-2,
+        // ...
+      }
+    }
+  ],
+  meta: {
+    // ...
+  }
+}
+
+// strapi-parse Output
+[
+    {
+        title: Test Article,
+        slug: test-article,
+        // ...
+    },
+    {
+        title: Test Article 2,
+        slug: test-article-2,
+        // ...
+    }
+]
+```
+### Single Type
+```javascript
+// Strapi API Response
+var strapiAPIResponse = {
+    data: {
+        id: 1,
+        attributes: {
+            title: Test Article,
+            slug: test-article,
+            // ...
+        },
+        meta: {
+            availableLocales: []
+        }
+    },
+    meta: { }
+}
+
+// strapi-parse Output
+{
+    title: Test Article,
+    slug: test-article,
+    // ...
+}
+```
+
+___
+## Author
+Joshua Henry - [@jch-code](https://github.com/jch-code)
+
+## License
+[MIT License](https://github.com/jch-code/strapi-parse/blob/main/LICENSE)
---
+++ strapi-parse.js
@@ -0,0 +1,159 @@
+/*
+Collections types in Strapi return in the following format
+  {
+    data [
+    {
+      attributes: {
+        createdAt: ""
+        updatedAt: ""
+        },
+      id: 0
+    },
+      ...
+    ]
+  }
+
+Single types in Strapi return in the following format
+{
+  data {
+    attributes: {
+      createdAt: ""
+      updatedAt: ""
+    },
+    id: 0
+  }
+}
+
+Components in Strapi do not follow the above format, but instead 
+return as a simple object, Ex:
+  {
+    id: 0
+    title: ""
+    description: ""
+  }
+
+Dynamic Zones in Strapi also do not follow the standard content
+type format, but instead return as a simple array of components
+  [
+    {
+      id: 0
+      title: ""
+      description: ""
+    },
+    ...
+  ]
+*/
+function strapiParse(strapiResponse) {
+  if (Array.isArray(strapiResponse.data))
+    return parseCollection(strapiResponse);
+
+  return parseSingle(strapiResponse);
+}
+
+function parseCollection(collection) {
+  /*
+   If collection.data is undefined, this must be a dynamic zone, 
+   so we set data to collection instead of collection.data
+  */
+  var data = collection.data || collection;
+
+  data = data.map(entity => {
+
+    var obj = {
+      id: entity.id
+    }
+
+    /*
+     If entity.attributes is undefined, this mist be a component,
+     so we create an attributes field on the object and set it to the 
+     object itself.
+
+     This is done so the component is compatible with the parsing logic below
+     that was written for the standard format.
+    */
+    if (!entity.attributes) {
+      entity = {
+        attributes: entity
+      }
+    }
+
+    var attributes = Object.getOwnPropertyNames(entity.attributes)
+
+    attributes.forEach(attr => {
+      if (entity.attributes[attr] == null) {
+        obj[attr] = null
+      } else {
+
+        obj[attr] = strapiParse(entity.attributes[attr])
+
+        /*
+         If the attrbute is a one-choice Strapi relationship, and the 
+         relationship is not set, the format is
+         
+          {
+            data: null
+          }
+
+          In this case, we just set the property to null.
+
+          If the attrbute is a multiple-choice Strapi relationship, and the 
+          relationship is not set, the format is
+         
+          {
+            data: []
+          }
+        */
+        if (entity.attributes[attr].data === null) {
+          obj[attr] = null;
+        }
+      }
+    });
+
+    return obj
+  })
+
+  return data;
+}
+
+function parseSingle(entity) {
+  /*
+   If the entity does not have the data property, it is either
+   a Strapi component or just an attribute field so we return the entity.
+  */
+  if (!entity.data) return entity
+
+  var attributes = Object.getOwnPropertyNames(entity.data.attributes)
+
+  var obj = {
+    id: entity.id
+  }
+
+  attributes.forEach(attr => {
+    if (entity.data.attributes[attr] == null) {
+      obj[attr] = null
+    } else {
+      obj[attr] = strapiParse(entity.data.attributes[attr])
+
+      /*
+         If the attrbute is a 'Has One' Strapi relationship, and the 
+         relationship is not set, the format is
+         
+          {
+            data: null
+          }
+
+          In this case, we just set the property to null.
+        */
+      if (entity.data.attributes[attr].data === null) {
+        obj[attr] = null;
+      }
+    }
+  });
+
+  return obj
+}
+
+module.exports = {
+  strapiParse,
+  default: strapiParse
+}
